9b97fc4100733403f3f6b7cdc7d3877f78190ca0
Liam Simmons <liam.simmons@powerhrg.com>
2024-09-25 11:00:49 -0400

    Stove Changes

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 pin "application"
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.es.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
